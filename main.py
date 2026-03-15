17e381056a10b7e8622d7d5721decf6bb6feeff3
zaanetaa <zanetaszustakiewicz@outlook.com>
2026-03-15 16:29:41 +0100

    task_1: ask about age

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-def print_hi(name):
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Zaimplementuj proszę kod, w którym będziesz pobierać z konsoli wiek użytkownika
+# a następnie wypiszesz na konsolę hej! Twoj wiek to <to co wpisał użytkownik>
+#  Za <to co wpisał użytkownik> oczywiście podstaw zmienną wieku użytkownika.
+# (Pobieranie z konsoli = wczytanie wartości z klawiatury:))
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# podstawowe zapytanie
 
+age = input("Ile masz lat? ")
+print(f"Hej, Twój wiek to {age}")
 
+# funkcja
+def age_question():
+    age1 = input("Ile masz lat? ")
+    print(print(f"Hej, Twój wiek to {age}"))
+
+age_question()
