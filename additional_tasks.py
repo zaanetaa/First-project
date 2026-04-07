e6992e431ed71386d1ca5c468dcc691924f64a85
zaanetaa <zanetaszustakiewicz@outlook.com>
2026-04-07 20:41:12 +0200

    fixed file additional_tasks by added definitions

--- additional_tasks.py
+++ additional_tasks.py
@@ -1,51 +1,56 @@
-# random_number = int(input("Podaj jakąś liczbę całkowitą: "))
-# x2random_number = 2 * random_number
-# if x2random_number > 100:
-#     numberminus100 = x2random_number - 50
-#     print(numberminus100)
-# else:
-#     print(x2random_number)
-#
-#______________________________________________________________
-# random_number = int(input("Podaj jakąś liczbę całkowitą: "))
-# if random_number % 2 == 0:
-#     print("Jest podzielna przez 2")
+def task_1():
+    random_number = int(input("Podaj jakąś liczbę całkowitą: "))
+    x2random_number = 2 * random_number
+    if x2random_number > 100:
+        numberminus100 = x2random_number - 50
+        print(numberminus100)
+    else:
+        print(x2random_number)
+# task_1()
 
-#______________________________________________________
-# random_number = int(input("Podaj jakąś liczbę całkowitą: "))
-# if random_number % 2 == 0:
-#     print("Jest podzielna przez 2")
-# else:
-#     print("Nie jest podzielna przez 2")
+def task_2():
+    random_number = int(input("Podaj jakąś liczbę całkowitą: "))
+    if random_number % 2 == 0:
+        print("Jest podzielna przez 2")
+# task_2()
 
-#___________________________________________
-# random_number = int(input("Podaj jakąś liczbę całkowitą: "))
-# if random_number % 2 == 0 or random_number % 3 == 0:
-#     print("Jest podzielna przez 2 lub 3")
-# _________________________________________
+def task_3():
+    random_number = int(input("Podaj jakąś liczbę całkowitą: "))
+    if random_number % 2 == 0:
+        print("Jest podzielna przez 2")
+    else:
+        print("Nie jest podzielna przez 2")
+# task_3()
 
-# random_number = int(input("Podaj jakąś liczbę całkowitą: "))
-# if random_number % 2 == 0 or random_number % 3 == 0:
-#     print("Jest podzielna przez 2 lub 3")
-# elif random_number % 5 == 0:
-#     print("Jest podzielna przez 5")
-# elif random_number % 7 == 0:
-#     print("Jest podzielna przez 7")
-# ___________________________________
+def task_4():
+    random_number = int(input("Podaj jakąś liczbę całkowitą: "))
+    if random_number % 2 == 0 or random_number % 3 == 0:
+        print("Jest podzielna przez 2 lub 3")
+#task_4()
 
-# random_number = int(input("Podaj jakąś liczbę całkowitą: "))
-# if random_number % 5 == 0 or random_number % 7 == 0:
-#     print("Jest podzielna przez 5 lub 7")
-# elif random_number % 5 == 0:
-#     print("Jest podzielna przez 5")
-# elif random_number % 7 == 0:
-#     print("Jest podzielna przez 7")
-# ________________________________________
+def task_5():
+    random_number = int(input("Podaj jakąś liczbę całkowitą: "))
+    if random_number % 2 == 0 or random_number % 3 == 0:
+        print("Jest podzielna przez 2 lub 3")
+    elif random_number % 5 == 0:
+        print("Jest podzielna przez 5")
+    elif random_number % 7 == 0:
+        print("Jest podzielna przez 7")
+#task_5()
+def task_6():
+    random_number = int(input("Podaj jakąś liczbę całkowitą: "))
+    if random_number % 5 == 0 or random_number % 7 == 0:
+        print("Jest podzielna przez 5 lub 7")
+    elif random_number % 5 == 0:
+        print("Jest podzielna przez 5")
+    elif random_number % 7 == 0:
+        print("Jest podzielna przez 7")
+#task_6()
 
-
-random_number = int(input("Podaj jakąś liczbę całkowitą: "))
-if random_number % 5 == 0:
-    print("Jest podzielna przez 5")
+def task_7():
+    random_number = int(input("Podaj jakąś liczbę całkowitą: "))
+    if random_number % 5 == 0:
+        print("Jest podzielna przez 5")
     random_number_minus_5 = random_number - 5
     if random_number_minus_5 < 0:
         print("Liczba po odjeciu 5 jest mniejsza od 0")
@@ -53,3 +58,4 @@ if random_number % 5 == 0:
         print("Liczba po odjeciu 5 jest równa 0")
     elif random_number_minus_5 > 0:
         print("Liczba po odjeciu 5 jest większa od 0")
+task_7()
