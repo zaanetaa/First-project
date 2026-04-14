e5ac9569c27b88a062629b3d9ada8c3c0cdd8d5e
zaanetaa <zanetaszustakiewicz@outlook.com>
2026-04-14 20:07:14 +0200

    added if dependencies

--- task_4_loop_for.py
+++ task_4_loop_for.py
@@ -1,6 +1,6 @@
 Magda_points =[6, 7, 0, 6, 0, 6, 4, 0, 7]
 Przemek_points = [0, 5, 6, 2, 0, 4, 0, 6, 9]
-# Runda = range (10)
+
 Magda_suma = 0
 Przemek_suma = 0
 for round in range (len(Magda_points)):
@@ -9,3 +9,9 @@ for round in range (len(Magda_points)):
     print(f"Runda {round +1} \nMagda ilość punktów {Magda_points[round]}"
           f"\nPrzemek ilość punktów {Przemek_points[round]}")
     print(f"Suma Magdy: {Magda_suma}\nPrzemek_suma: {Przemek_suma}")
+if Magda_suma > Przemek_suma:
+    print("Gre wygrała Magda")
+elif Przemek_suma > Magda_suma:
+    print("Gre wygrał Przemek")
+else:
+    print("Był remis")
